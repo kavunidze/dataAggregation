626084cd9e7929d4077654468254d05cade1d3d9
kavunidze <kavunidze@protonmail.com>
2024-05-08 19:08:43 +0300

    update settings

---
+++ MainApp/Model/Chatter.cs
@@ -0,0 +1,30 @@
+﻿using MainApp.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Model
+{
+    class Chatter
+    {
+        public AbstractChat _Chat { get; set; }
+        public string Name { get; set; }
+
+        public Chatter(string name)
+        {
+            Name = name;
+        }
+
+        public void Send(string to, string msg)
+        {
+            _Chat.Send(Name, to, msg);
+        }
+
+        public virtual void Receive(string from, string msg)
+        {
+            Console.WriteLine("{0} to {1}: '{2}'", from, Name, msg);
+        }
+    }
+}
