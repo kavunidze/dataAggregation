43e507c9d2afab5eb54dfb1de2c5752ad9ca042c
kavunidze <kavunidze@protonmail.com>
2024-05-08 19:08:43 +0300

    add new styles

---
+++ MainApp/Model/FrontEndDev.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Model
+{
+    class FrontEndDev : Chatter
+    {
+        public FrontEndDev(string name) : base(name)
+        {
+        }
+
+        public override void Receive(string from, string msg)
+        {
+            Console.Write("To Front-end Dev: ");
+            base.Receive(from, msg);
+        }
+    }
+}
