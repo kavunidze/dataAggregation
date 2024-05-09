0a6d54eec605868a334f300c03f6f4d3e68a5e7c
kavunidze <kavunidze@protonmail.com>
2024-05-09 19:08:43 +0300

    Updated settings

---
+++ MainApp/Mediator/AbstractChat.cs
@@ -0,0 +1,17 @@
+﻿using MainApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Mediator
+{
+    abstract class AbstractChat
+    {
+        public abstract void Register(Chatter c);
+
+        public abstract void Send(string from, string to, string msg);
+        
+    }
+}
