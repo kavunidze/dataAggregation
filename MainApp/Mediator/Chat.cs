c242fc517f04b5ab28b8dbcbb6391f2640dd945c
kavunidze <kavunidze@protonmail.com>
2024-05-09 19:08:43 +0300

    update readme

---
+++ MainApp/Mediator/Chat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainApp.Model;
+
+namespace MainApp.Mediator
+{
+    class Chat : AbstractChat
+    {
+        private Dictionary<string, Chatter> _chatters =
+            new Dictionary<string, Chatter>();
+
+        public override void Register(Chatter c)
+        {
+            if (!_chatters.ContainsValue(c))
+            {
+                _chatters[c.Name] = c;
+            }
+            c._Chat = this;
+        }
+
+        public override void Send(string from, string to, string msg)
+        {
+            Chatter chatter = _chatters[to];
+            if (chatter != null)
+            {
+                chatter.Receive(from, msg);
+            }
+        }
+
+    }
+}
