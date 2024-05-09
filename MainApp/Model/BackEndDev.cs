0ad8593066a64c521988f882e69fed9996c527d6
kavunidze <kavunidze@protonmail.com>
2024-05-09 19:08:43 +0300

    add new icons

---
+++ MainApp/Model/BackEndDev.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Model
+{
+    class BackEndDev : Chatter
+    {
+        public BackEndDev(string name) : base(name)
+        {
+        }
+        public override void Receive(string from, string msg)
+        {
+            Console.Write("To Back-end Dev: ");
+            base.Receive(from, msg);
+        }
+    }
+}
