19ca1c840329eb83b6a87446f6de15f2c2a438ff
kavunidze <kavunidze@protonmail.com>
2024-05-12 19:08:43 +0300

    Update config settings

---
+++ MainApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using MainApp.Mediator;
+using MainApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chat chat = new Chat();
+
+            Chatter Asmodei = new FrontEndDev("Asmodei");
+            Chatter Boryssey = new FrontEndDev("Boryssey");
+            Chatter Igor = new BackEndDev("Igor");
+            Chatter Idk = new BackEndDev("Idk");
+
+            chat.Register(Asmodei);
+            chat.Register(Boryssey);
+            chat.Register(Igor);
+            chat.Register(Idk);
+
+            Asmodei.Send("Boryssey", "Privet");
+            Boryssey.Send("Asmodei", "Nu privet");
+            Igor.Send("Idk", "Who are you?");
+            Idk.Send("Igor", "Idk!");
+            Console.ReadKey();
+        }
+    }
+}
